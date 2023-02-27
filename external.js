98df40bb2c4de8859c31bb919a66e70f30581df2
jpyeon <jpyeon1216@gmail.com>
2023-02-27 15:24:51 -0500

    initial

---
+++ external.js
@@ -0,0 +1,11 @@
+// Define the Size of the SVG Container
+const width = 500;
+const height = 500;
+const margin = {left: 50, right: 50, top: 50, bottom: 50};
+const circle_height = height - margin.top - margin.bottom;
+const circle_width = width - margin.left - margin.right;
+
+
+
+
+
